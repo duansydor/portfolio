3f34137d865d09a6163096071fbf16f2f68a8179
duansydor <josiasduan@gmail.com>
2019-05-11 13:51:29 -0300

    Hello World

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Footer(props){
+	return(
+		<footer className="app-header">
+			
+		</footer>
+	);
+}
+export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-function Header(){
+function Header(props){
 	return(
 		<header className="app-header">
 			<div className="hamburger"></div>
+			<h1>{ props.title }</h1>
 		</header>
 	);
 }
