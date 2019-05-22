4e3f9eb1ff0486b0d62a7e722bba80a59eeb0625
duansydor <josiasduan@gmail.com>
2019-05-22 18:16:22 -0300

    Updates

---
+++ precache-manifest.bb5441cfb000057d8412e9e3dd435191.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34acb92eedfd7aff8fdbe035779c72a1",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "9c954bac531bd44d2ab7",
+    "url": "/myportfolio/static/css/main.9c08864a.chunk.css"
+  },
+  {
+    "revision": "b3e196a33e043383e27b",
+    "url": "/myportfolio/static/js/2.63e48751.chunk.js"
+  },
+  {
+    "revision": "9c954bac531bd44d2ab7",
+    "url": "/myportfolio/static/js/main.770b648b.chunk.js"
+  },
+  {
+    "revision": "314136164c1c0c643422",
+    "url": "/myportfolio/static/js/runtime~main.69c47bdd.js"
+  },
+  {
+    "revision": "2a73ee7b7d55261739a295b529951862",
+    "url": "/myportfolio/static/media/card.2a73ee7b.jpg"
+  },
+  {
+    "revision": "d8dc03991b5843a9cf95b1f39427d2b9",
+    "url": "/myportfolio/static/media/profile.d8dc0399.png"
+  }
+]);
