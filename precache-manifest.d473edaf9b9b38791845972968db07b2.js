414c906da417f012ffa9494ac69ddfddbd65c47d
duansydor <josiasduan@gmail.com>
2019-05-22 19:57:23 -0300

    Updates

---
+++ precache-manifest.d473edaf9b9b38791845972968db07b2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aa1e569e6e2a107fca6eb6a2ee2d284",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "c1f3375fea3f54453380",
+    "url": "/myportfolio/static/css/main.15ad3579.chunk.css"
+  },
+  {
+    "revision": "505dba6ae727341437cf",
+    "url": "/myportfolio/static/js/2.d7360152.chunk.js"
+  },
+  {
+    "revision": "c1f3375fea3f54453380",
+    "url": "/myportfolio/static/js/main.a348e395.chunk.js"
+  },
+  {
+    "revision": "314136164c1c0c643422",
+    "url": "/myportfolio/static/js/runtime~main.69c47bdd.js"
+  },
+  {
+    "revision": "2a73ee7b7d55261739a295b529951862",
+    "url": "/myportfolio/static/media/card.2a73ee7b.jpg"
+  },
+  {
+    "revision": "57de78e75e7d28a4c18dea3b998b668e",
+    "url": "/myportfolio/static/media/git.57de78e7.png"
+  },
+  {
+    "revision": "673cd9b612aa02a493ca0d752dc40099",
+    "url": "/myportfolio/static/media/insta.673cd9b6.png"
+  },
+  {
+    "revision": "d8dc03991b5843a9cf95b1f39427d2b9",
+    "url": "/myportfolio/static/media/profile.d8dc0399.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myportfolio/precache-manifest.bb5441cfb000057d8412e9e3dd435191.js"
+  "/myportfolio/precache-manifest.d473edaf9b9b38791845972968db07b2.js"
 );
 
 self.addEventListener('message', (event) => {
